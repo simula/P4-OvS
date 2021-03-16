02a4fe04bc5029c057d27595dec00c2ec4933b26
Thomas Dreibholz <dreibh@simula.no>
2021-03-16 13:59:38 +0100

    Updated build files.

--- debian/automake.mk
+++ debian/automake.mk
@@ -5,35 +5,55 @@ EXTRA_DIST += \
 	debian/copyright \
 	debian/copyright.in \
 	debian/dirs \
+	debian/dkms.conf.in \
+	debian/gbp.conf \
+	debian/ifupdown.sh \
 	debian/openvswitch-p4-common.dirs \
 	debian/openvswitch-p4-common.install \
+	debian/openvswitch-p4-doc.doc-base \
+	debian/openvswitch-p4-doc.install \
 	debian/openvswitch-p4-pki.dirs \
 	debian/openvswitch-p4-pki.postinst \
 	debian/openvswitch-p4-pki.postrm \
-	debian/openvswitch-p4-switch.README.Debian \
+	debian/openvswitch-p4-source.dirs \
+	debian/openvswitch-p4-source.install \
+	debian/openvswitch-p4-switch.default \
 	debian/openvswitch-p4-switch.dirs \
+	debian/openvswitch-p4-switch-dpdk.postinst \
+	debian/openvswitch-p4-switch-dpdk.prerm \
+	debian/openvswitch-p4-switch-dpdk.README.Debian \
 	debian/openvswitch-p4-switch.init \
 	debian/openvswitch-p4-switch.install \
+	debian/openvswitch-p4-switch.links \
 	debian/openvswitch-p4-switch.logrotate \
+	debian/openvswitch-p4-switch.ovsdb-server.service \
+	debian/openvswitch-p4-switch.ovs-vswitchd.service \
 	debian/openvswitch-p4-switch.postinst \
 	debian/openvswitch-p4-switch.postrm \
-	debian/openvswitch-p4-switch.default \
-	debian/openvswitch-p4-switch.links \
-	debian/openvswitch-p4-test.install \
-	debian/openvswitch-p4-testcontroller.README.Debian \
+	debian/openvswitch-p4-switch.prerm \
+	debian/openvswitch-p4-switch.README.Debian \
+	debian/openvswitch-p4-switch.service \
 	debian/openvswitch-p4-testcontroller.default \
 	debian/openvswitch-p4-testcontroller.dirs \
 	debian/openvswitch-p4-testcontroller.init \
 	debian/openvswitch-p4-testcontroller.install \
 	debian/openvswitch-p4-testcontroller.postinst \
 	debian/openvswitch-p4-testcontroller.postrm \
+	debian/openvswitch-p4-testcontroller.README.Debian \
+	debian/openvswitch-p4-test.install \
 	debian/openvswitch-p4-vtep.default \
 	debian/openvswitch-p4-vtep.dirs \
 	debian/openvswitch-p4-vtep.init \
 	debian/openvswitch-p4-vtep.install \
+	debian/ovs-systemd-reload \
+	debian/patches/series \
 	debian/rules \
-	debian/ifupdown.sh \
-	debian/source/format
+	debian/rules.modules \
+	debian/source/format \
+	debian/tests/control \
+	debian/tests/dpdk \
+	debian/tests/openflow.py \
+	debian/tests/vanilla
 
 check-debian-changelog-version:
 	@DEB_VERSION=`echo '$(VERSION)' | sed 's/pre/~pre/'`;		     \
